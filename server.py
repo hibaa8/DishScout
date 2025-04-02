391ce8f2070a9a732e4760af2234132048f359c7
Afsana Rahman <ar4334@columbia.edu>
2025-04-01 23:21:43 -0400

    Finalized front-end templates and test routes

--- server.py
+++ server.py
@@ -1,222 +1,98 @@
+from flask import Flask, request, render_template
 
-"""
-Columbia's COMS W4111.001 Introduction to Databases
-Example Webserver
-To run locally:
-    python server.py
-Go to http://localhost:8111 in your browser.
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
-import os
-  # accessible as a variable in index.html:
-from sqlalchemy import *
-from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+app = Flask(__name__)
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
-
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@34.148.223.31/proj1part2
-#
-# For example, if you had username zy2431 and password 123123, then the following line would be:
-#
-#     DATABASEURI = "postgresql://zy2431:123123@34.148.223.31/proj1part2"
-#
-# Modify these with your own credentials you received from TA!
-DATABASE_USERNAME = ""
-DATABASE_PASSWRD = ""
-DATABASE_HOST = "34.148.223.31"
-DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/proj1part2"
-
-
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
-engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-with engine.connect() as conn:
-	create_table_command = """
-	CREATE TABLE IF NOT EXISTS test (
-		id serial,
-		name text
-	)
-	"""
-	res = conn.execute(text(create_table_command))
-	insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
-	res = conn.execute(text(insert_table_command))
-	# you need to commit for create, insert, update queries to reflect
-	conn.commit()
-
-
-@app.before_request
-def before_request():
-	"""
-	This function is run at the beginning of every web request 
-	(every time you enter an address in the web browser).
-	We use it to setup a database connection that can be used throughout the request.
-
-	The variable g is globally accessible.
-	"""
-	try:
-		g.conn = engine.connect()
-	except:
-		print("uh oh, problem connecting to database")
-		import traceback; traceback.print_exc()
-		g.conn = None
-
-@app.teardown_request
-def teardown_request(exception):
-	"""
-	At the end of the web request, this makes sure to close the database connection.
-	If you don't, the database could run out of memory!
-	"""
-	try:
-		g.conn.close()
-	except Exception as e:
-		pass
-
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: https://flask.palletsprojects.com/en/1.1.x/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-	"""
-	request is a special object that Flask provides to access web request information:
+    return render_template("index.html")
 
-	request.method:   "GET" or "POST"
-	request.form:     if the browser submitted a form, this contains the data in the form
-	request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+@app.route('/search')
+def search():
+    query = request.args.get("query")
+    search_type = request.args.get("search_type")
 
-	See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
-	"""
+    # Simulated dummy data
+    if search_type == "food":
+        results = [
+            {"food_item_id": 1, "name": "Spicy Ramen", "restaurant_name": "Totto Ramen"},
+            {"food_item_id": 2, "name": "Miso Soup", "restaurant_name": "Ippudo"}
+        ]
+    else:
+        results = [
+            {"restaurant_id": 101, "name": "Totto Ramen", "location": "NYC"},
+            {"restaurant_id": 102, "name": "Ippudo", "location": "NYC"}
+        ]
 
-	# DEBUG: this is debugging code to see what request looks like
-	print(request.args)
+    return render_template("search_results.html", query=query, search_type=search_type, results=results)
 
+@app.route('/food_item/<int:food_item_id>')
+def food_item(food_item_id):
+    # Fake food item info
+    food = {
+        "food_item_id": food_item_id,
+        "name": "Spicy Ramen",
+        "restaurant_id": 101,
+        "restaurant_name": "Totto Ramen",
+        "nutritional_info": "600 cal, 20g protein, 70g carbs",
+        "avg_rating": 4.5
+    }
 
-	#
-	# example of a database query
-	#
-	select_query = "SELECT name from test"
-	cursor = g.conn.execute(text(select_query))
-	names = []
-	for result in cursor:
-		names.append(result[0])
-	cursor.close()
+    # Fake reviews
+    reviews = [
+        {
+            "taste_rating": 5,
+            "presentation_rating": 4,
+            "price_rating": 4,
+            "value_rating": 5,
+            "comment": "Delicious and super flavorful!"
+        },
+        {
+            "taste_rating": 4,
+            "presentation_rating": 3,
+            "price_rating": 4,
+            "value_rating": 4,
+            "comment": "Good but a bit salty."
+        }
+    ]
 
-	#
-	# Flask uses Jinja templates, which is an extension to HTML where you can
-	# pass data to a template and dynamically generate HTML based on the data
-	# (you can think of it as simple PHP)
-	# documentation: https://realpython.com/primer-on-jinja-templating/
-	#
-	# You can see an example template in templates/index.html
-	#
-	# context are the variables that are passed to the template.
-	# for example, "data" key in the context variable defined below will be 
-	# accessible as a variable in index.html:
-	#
-	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-	#     <div>{{data}}</div>
-	#     
-	#     # creates a <div> tag for each element in data
-	#     # will print: 
-	#     #
-	#     #   <div>grace hopper</div>
-	#     #   <div>alan turing</div>
-	#     #   <div>ada lovelace</div>
-	#     #
-	#     {% for n in data %}
-	#     <div>{{n}}</div>
-	#     {% endfor %}
-	#
-	context = dict(data = names)
+    return render_template("food_item.html", food=food, reviews=reviews)
 
+@app.route('/add_review/<int:food_item_id>', methods=['GET', 'POST'])
+def add_review(food_item_id):
+    if request.method == 'POST':
+        # Normally you'd save the review to the DB here.
+        print("Review Submitted:")
+        print("Taste:", request.form.get("taste"))
+        print("Presentation:", request.form.get("presentation"))
+        print("Price:", request.form.get("price"))
+        print("Value:", request.form.get("value"))
+        print("Comment:", request.form.get("comment"))
 
-	#
-	# render_template looks in the templates/ folder for files.
-	# for example, the below file reads template/index.html
-	#
-	return render_template("index.html", **context)
+        return "Review submitted! (You can redirect to food page here.)"
 
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-	return render_template("another.html")
+    # Dummy name for now
+    return render_template("add_review.html", food_item_id=food_item_id, food_name="Spicy Ramen")
 
+@app.route('/restaurant/<int:restaurant_id>')
+def restaurant(restaurant_id):
+    restaurant = {
+        "name": "Totto Ramen",
+        "location": "366 W 52nd St, New York, NY",
+        "phone_number": "212-582-0052",
+        "hours": "Mon-Sun: 11:00AM – 11:00PM",
+        "website": "https://tottoramen.com"
+    }
 
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-	# accessing form inputs from user
-	name = request.form['name']
-	
-	# passing params in for each variable into query
-	params = {}
-	params["new_name"] = name
-	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-	g.conn.commit()
-	return redirect('/')
+    menu_items = [
+        {"food_item_id": 1, "name": "Spicy Ramen"},
+        {"food_item_id": 2, "name": "Miso Soup"},
+        {"food_item_id": 3, "name": "Chicken Karaage"}
+    ]
 
+    return render_template("restaurant.html", restaurant=restaurant, menu_items=menu_items)
 
-@app.route('/login')
-def login():
-	abort(401)
-	this_is_never_executed()
 
 
 if __name__ == "__main__":
-	import click
+    app.run(debug=True, port=8111)
 
-	@click.command()
-	@click.option('--debug', is_flag=True)
-	@click.option('--threaded', is_flag=True)
-	@click.argument('HOST', default='0.0.0.0')
-	@click.argument('PORT', default=8111, type=int)
-	def run(debug, threaded, host, port):
-		"""
-		This function handles command line parameters.
-		Run the server using:
-
-			python server.py
-
-		Show the help text using:
-
-			python server.py --help
-
-		"""
-
-		HOST, PORT = host, port
-		print("running on %s:%d" % (HOST, PORT))
-		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-
-run()
