fa66ddf94fecc036394cdab2a88856bcc7abf521
hibaa8 <hibaaltaf98@gmail.com>
2025-04-06 21:30:10 -0400

    starter server

--- server.py
+++ server.py
@@ -30,33 +30,21 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #     DATABASEURI = "postgresql://zy2431:123123@34.148.223.31/proj1part2"
 #
 # Modify these with your own credentials you received from TA!
-DATABASE_USERNAME = ""
-DATABASE_PASSWRD = ""
+DATABASE_USERNAME = "ha2616"
+DATABASE_PASSWRD = "489057"
 DATABASE_HOST = "34.148.223.31"
 DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/proj1part2"
+
+# DATABASE_USERNAME = ""
+# DATABASE_PASSWRD = ""
+# DATABASE_HOST = "34.148.223.31"
+# DATABASEURI = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWRD}@{DATABASE_HOST}/proj1part2"
 
 
 #
 # This line creates a database engine that knows how to connect to the URI above.
 #
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-with engine.connect() as conn:
-	create_table_command = """
-	CREATE TABLE IF NOT EXISTS test (
-		id serial,
-		name text
-	)
-	"""
-	res = conn.execute(text(create_table_command))
-	insert_table_command = """INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace')"""
-	res = conn.execute(text(insert_table_command))
-	# you need to commit for create, insert, update queries to reflect
-	conn.commit()
 
 
 @app.before_request
@@ -87,111 +75,58 @@ def teardown_request(exception):
 		pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: https://flask.palletsprojects.com/en/1.1.x/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-	"""
-	request is a special object that Flask provides to access web request information:
+    return render_template("index.html")
 
-	request.method:   "GET" or "POST"
-	request.form:     if the browser submitted a form, this contains the data in the form
-	request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-	See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
-	"""
-
-	# DEBUG: this is debugging code to see what request looks like
-	print(request.args)
-
-
-	#
-	# example of a database query
-	#
-	select_query = "SELECT name from test"
-	cursor = g.conn.execute(text(select_query))
-	names = []
-	for result in cursor:
-		names.append(result[0])
-	cursor.close()
-
-	#
-	# Flask uses Jinja templates, which is an extension to HTML where you can
-	# pass data to a template and dynamically generate HTML based on the data
-	# (you can think of it as simple PHP)
-	# documentation: https://realpython.com/primer-on-jinja-templating/
-	#
-	# You can see an example template in templates/index.html
-	#
-	# context are the variables that are passed to the template.
-	# for example, "data" key in the context variable defined below will be 
-	# accessible as a variable in index.html:
-	#
-	#     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-	#     <div>{{data}}</div>
-	#     
-	#     # creates a <div> tag for each element in data
-	#     # will print: 
-	#     #
-	#     #   <div>grace hopper</div>
-	#     #   <div>alan turing</div>
-	#     #   <div>ada lovelace</div>
-	#     #
-	#     {% for n in data %}
-	#     <div>{{n}}</div>
-	#     {% endfor %}
-	#
-	context = dict(data = names)
-
-
-	#
-	# render_template looks in the templates/ folder for files.
-	# for example, the below file reads template/index.html
-	#
-	return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-	return render_template("another.html")
 
 
 # Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-	# accessing form inputs from user
-	name = request.form['name']
+# @app.route('/add', methods=['POST'])
+# def add():
+# 	# accessing form inputs from user
+# 	name = request.form['name']
 	
-	# passing params in for each variable into query
-	params = {}
-	params["new_name"] = name
-	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
-	g.conn.commit()
-	return redirect('/')
+# 	# passing params in for each variable into query
+# 	params = {}
+# 	params["new_name"] = name
+# 	g.conn.execute(text('INSERT INTO test(name) VALUES (:new_name)'), params)
+# 	g.conn.commit()
+# 	return redirect('/')
+
+@app.route('/search', methods=['POST'])
+def search():
+    food_name = request.form("query")
+    # search_type = request.args.get("search_type")
+    get_food_item_query = f"""
+    WITH selected_food_item AS (
+    SELECT food_item_id
+    FROM rating
+    WHERE food_item_id IN (
+        SELECT food_item_id 
+        FROM food_item 
+        WHERE name ILIKE '%{food_name}%'
+    )
+    GROUP BY food_item_id
+    ORDER BY SUM(taste_rating + presentation_rating + price_rating + value_rating) DESC
+    LIMIT 1
+    )
+    SELECT rest.name, rest.location 
+    FROM restaurant rest
+    JOIN selected_food_item food ON rest.restaurant_id = food.restaurant_id
+    WHERE food.food_item_id = (SELECT food_item_id FROM selected_food_item);
+    """
+    cursor = g.conn.execute(text(get_food_item_query))
+    options = []
+    for result in cursor:
+        options.append(result[0])
+    cursor.close()
+
+    context = dict(data = options)
+
+    return render_template("search_results.html", query=food_name, results=context)
 
 
-@app.route('/login')
-def login():
-	abort(401)
-	this_is_never_executed()
 
 
 if __name__ == "__main__":
